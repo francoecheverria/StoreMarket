c0372f767ed6a40db0491905eaf20b62d913ea90
Franco Echeverria <fecheverria@penta.biz>
2026-08-22 01:09:18 -0300

    fix v2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -26,6 +26,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             'webhooks/mercadopago',
         ]);
+
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
